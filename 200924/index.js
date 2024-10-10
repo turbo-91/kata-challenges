e9d39b9b840b9e0604c5d3fde242ad9001fb0e22
Torben Jost <torbenjost@proton.me>
2024-10-10 09:37:46 +0200

    kata challenges 200924

---
+++ 200924/index.js
@@ -0,0 +1,14 @@
+function rps(p1, p2) {
+  const scissors = "scissors";
+  const rock = "rock";
+  const paper = "paper";
+  if (p1 === scissors && p2 === paper) return "Player 1 won!";
+  if (p1 === scissors && p2 === rock) return "Player 2 won!";
+  if (p1 === scissors && p2 === scissors) return "Draw!";
+  if (p1 === rock && p2 === scissors) return "Player 1 won!";
+  if (p1 === rock && p2 === paper) return "Player 2 won!";
+  if (p1 === rock && p2 === rock) return "Draw!";
+  if (p1 === paper && p2 === rock) return "Player 1 won!";
+  if (p1 === paper && p2 === scissors) return "Player 2 won!";
+  if (p1 === paper && p1 === paper) return "Draw!";
+}
